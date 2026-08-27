16e3593819c4c6f4255947e94a7c540c79735163
louisecassidee <cassidy.panganiban13@gmail.com>
2026-08-27 09:18:42 +0800

    Update q1_sg5_a1_arayat_panganiban.py

--- Q1/q1_sg5_a1_arayat_panganiban.py
+++ Q1/q1_sg5_a1_arayat_panganiban.py
@@ -1,1 +1,19 @@
 
+# The RPG Hero
+
+class Hero:
+     def __init__(self, name, hp):
+         self.name = name
+         self.hp = hp
+         
+     def take_damage(self, amount):
+         self.hp = self.hp - amount
+
+arthur = Hero("Arthur", 100)
+morgana = Hero("Morgana", 100)
+
+arthur.take_damage(10)
+print("Arthur's health:", arthur.hp)
+print("Morgana's health:", morgana.hp)
+     
+         
